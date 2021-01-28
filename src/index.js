54f6e815183e0fc7dfcc59c6511a17a018bd16f0
Stepa-Kuropatkin <igortrotsuk@gmail.com>
2021-01-28 22:20:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
+  if (
+      typeof array != "undefined" &&
+      array != null &&
+      array.length != null &&
+      array.length > 0
+  ) {
+      return array.sort((a, b) => a - b).slice(0, 1);
+  }
   return 0;
-}
+};
 
-exports.max = function max (array) {
+exports.max = function max(array) {
+  if (
+      typeof array != "undefined" &&
+      array != null &&
+      array.length != null &&
+      array.length > 0
+  ) {
+      return array.sort((a, b) => b - a).slice(0, 1);
+  }
   return 0;
-}
+};
 
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
+  if (
+      typeof array != "undefined" &&
+      array != null &&
+      array.length != null &&
+      array.length > 0
+  ) {
+      return array.reduce((a, b) => a + b, 0) / array.length;
+  }
   return 0;
-}
+};
